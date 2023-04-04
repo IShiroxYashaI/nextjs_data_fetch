bf6258db8fa0fce68ef5f65a8f736a882b9afa2b
Bali David <darkbalihun&gmail.com>
2023-04-04 17:59:03 +0200

    continued

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -16,7 +16,25 @@ export async function getAllEvents() {
     return events;
 }
 
-export function getFeaturedEvents() {
-    return DUMMY_EVENTS.filter((event) => event.isFeatured);
-  }
-  
+export async function getFilteredEvents(dateFilter) {
+    const { year, month } = dateFilter;
+
+    const allEvents = await getAllEvents();
+
+    let filteredEvents = allEvents.filter((event) => {
+        const eventDate = new Date(event.date);
+        return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
+    });
+    
+    return filteredEvents;
+}
+
+export async function getFeaturedEvents() {
+    const allEvents = await getAllEvents();
+    return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+    const allEvents = await getAllEvents();
+    return allEvents.find((event) => event.id === id);
+}
